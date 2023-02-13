123b33068463b71ae2bc61a7e23f1ff12229cbc4
Yanyan Zhou <zzsryy@qq.com>
2023-02-14 06:18:30 +0800

    Update Five In A Row.cpp

--- Five In A Row.cpp
+++ Five In A Row.cpp
@@ -23,12 +23,14 @@ const char P2 = 'X';
 
 char board[ROWS][COLS];
 
-void initBoard() //Initialize the character array board
+// Initialize the board with blank spaces
+void initBoard() 
 {
 	memset(board, ' ', sizeof(board));
 }
 
-void printBoard()   //output the board 
+// Print the current state of the board
+void printBoard()   
 {
 	cout << "  ";
 	for (int i = 0; i < COLS; i++) {
@@ -44,47 +46,52 @@ void printBoard()   //output the board
 	}
 }
 
+// Check if the move is valid
 bool isValidMove(int row, int col)
 {
 	return row >= 0 && row < ROWS&& col >= 0 && col < COLS&& board[row][col] == ' ';
 }
 
-bool hasWon(int row, int col, char player) //Determine if a player has met the win condition by traversing the board array
-{
-	int count = 0;
-	for (int i = row, j = col; i >= 0 && i < ROWS && j >= 0 && j < COLS && board[i][j] == player; i--, j--) {
-		count++;
-	}
-	for (int i = row + 1, j = col + 1; i >= 0 && i < ROWS && j >= 0 && j < COLS && board[i][j] == player; i++, j++) {
-		count++;
-	}
-	if (count >= 5) {
-		return true;
-	}
-	count = 0;
-	for (int i = row, j = col; i >= 0 && i < ROWS && j >= 0 && j < COLS && board[i][j] == player; i--, j++) {
-		count++;
-	}
-	for (int i = row + 1, j = col - 1; i >= 0 && i < ROWS && j >= 0 && j < COLS && board[i][j] == player; i++, j--) {
-		count++;
-	}
-	if (count >= 5) {
-		return true;
-	}
-	count = 0;
-	for (int i = row, j = col; i >= 0 && i < ROWS && j >= 0 && j < COLS && board[i][j] == player; i--) {
-		count++;
-	}
-	for (int i = row + 1; i >= 0 && i < ROWS && board[i][col] == player; i++) {
-		count++;
-	}
-	if (count >= 5) {
-		return true;
-	}
-	count = 0;
-	for (int j = col; j >= 0 && j < COLS && board[row][j] == player; j--) {
-		count++;
-	}
+// Check if the player has won after making a move
+bool hasWon(int row, int col, char player) {
+    int count = 0;
+    // Check horizontally
+    for (int i = col; i >= 0 && board[row][i] == player; i--) {
+        count++;
+    }
+    for (int i = col + 1; i < COLS && board[row][i] == player; i++) {
+        count++;
+    }
+    if (count >= 5) {
+        return true;
+    }
+    count = 0;
+    // Check vertically
+    for (int i = row; i >= 0 && board[i][col] == player; i--) {
+        count++;
+    }
+    for (int i = row + 1; i < ROWS && board[i][col] == player; i++) {
+        count++;
+    }
+    if (count >= 5) {
+        return true;
+    }
+    count = 0;
+    // Check diagonally (upper left to lower right)
+    for (int i = row, j = col; i >= 0 && j >= 0 && board[i][j] == player; i--, j--) {
+        count++;
+    }
+    for (int i = row + 1, j = col + 1; i < ROWS && j < COLS && board[i][j] == player; i++, j++) {
+        count++;
+    }
+    if (count >= 5) {
+        return true;
+    }
+    count = 0;
+    // Check diagonally (upper right to lower left)
+    for (int i = row, j = col; i >= 0 && j < COLS && board[i][j] == player; i--, j++) {
+        count++;
+    }
 	for (int j = col + 1; j >= 0 && j < COLS && board[row][j] == player; j--)
 	{
 		count++;
@@ -96,27 +103,27 @@ bool hasWon(int row, int col, char player) //Determine if a player has met the w
 }
 
 int main() {
-	initBoard();
-	char player = P1;
-	while (true) {
-		printBoard(); //output the board before and after player moves 
-		int row, col;
-		cout << "Player " << player << ", enter your move (row column): ";
-		cin >> row >> col;
-		cout << endl;
+    initBoard();
+    char player = P1;
+    while (true) {
+	printBoard(); //output the board before and after player moves 
+	int row, col;
+	cout << "Player " << player << ", enter your move (row column): ";
+	cin >> row >> col;
+	cout << endl;
 
-		while (!isValidMove(row, col)) //If the move is invalid, re-enter the move
-		{
-			cout << "Invalid move, try again: ";
-			cin >> row >> col;
-		}
-		board[row][col] = player;
-		if (hasWon(row, col, player)) //If a player achieves the winning conditions, the game ends.
-		{
-			cout << "Player " << player << " has won!" << endl;
-			break;
-		}
-		player = (player == P1) ? P2 : P1; //the player switches one by one
-	}
-	return 0;
+        while (!isValidMove(row, col)) //If the move is invalid, re-enter the move
+        {
+	       cout << "Invalid move, try again: ";
+	       cin >> row >> col;
+        }
+        board[row][col] = player;
+       if (hasWon(row, col, player)) //If a player achieves the winning conditions, the game ends.
+       {
+	       cout << "Player " << player << " has won!" << endl;
+	       break;
+       }
+       player = (player == P1) ? P2 : P1; //the player switches one by one
+    }
+    return 0;
 }
